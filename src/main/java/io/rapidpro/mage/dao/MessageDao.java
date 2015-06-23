08be60a3fbde847e2e9f855aa1ee2ee6e6769fee
Rowan Seymour <rowanseymour@gmail.com>
2015-06-23 10:22:21 +0200

    Don't initialize messages with msg_type INBOX

--- src/main/java/io/rapidpro/mage/dao/MessageDao.java
+++ src/main/java/io/rapidpro/mage/dao/MessageDao.java
@@ -81,7 +81,7 @@ public interface MessageDao {
 
     @SqlUpdate(
             "INSERT INTO " + Table.MESSAGE + " (channel_id, contact_id, contact_urn_id, text, direction, status, org_id, created_on, queued_on, has_template_error, msg_type, msg_count, external_id, error_count, next_attempt, visibility, priority) " +
-            "VALUES(:channelId, :contactId, :contactUrnId, :text, 'I', 'P', :orgId, :createdOn, :queuedOn, FALSE, 'I', 1, :externalId, 0, NOW(), 'V', :priority)"
+            "VALUES(:channelId, :contactId, :contactUrnId, :text, 'I', 'P', :orgId, :createdOn, :queuedOn, FALSE, NULL, 1, :externalId, 0, NOW(), 'V', :priority)"
     )
     @GetGeneratedKeys
     int insertIncoming(@Bind("channelId") Integer channelId,
